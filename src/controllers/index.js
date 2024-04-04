58decd1285fc87044d7c949914367c863055b8f2
jtula <joseph.tula@gmail.com>
2024-04-03 22:15:42 -0400

    feat: add controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,58 @@
+const pool = require("../db.js");
+
+const renderUser = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM user");
+    res.json({ users: rows });
+  } catch (error) {
+    res.status(500).send({success:false, message: error.message});
+  }
+};
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    await pool.query("INSERT INTO user (name, email) VALUES (?, ?)", [name, email]);
+    res.json({ 
+      message: "User created successfully",
+      user: {
+        name,
+        email,
+      },
+    });
+  } catch (error) {
+    res.status(500).send({success:false, message: error.message});
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  try {
+    await pool.query("UPDATE user set name = ?, email = ? WHERE id = ?", [name, email, id]);
+    res.json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(500).send({success:false, message: error.message});
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query("DELETE FROM user WHERE id = ?", [id]);
+  
+    if (result.affectedRows === 1) {
+      res.json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).send({success:false, message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send({success:false, message: error.message});
+  }
+};
+
+exports.renderUser = renderUser;
+exports.createUser = createUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
